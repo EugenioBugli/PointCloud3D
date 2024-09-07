d5e9e7540fea68cf9bdfaea368957a3e6956c86f
EugenioBugli <eugeniobugli15@gmail.com>
2024-09-07 15:39:58 +0200

    predictor

--- src/plane_predictor.py
+++ src/plane_predictor.py
@@ -63,4 +63,92 @@ class SimplePointNet(nn.Module):
         pre_pool_out_t = pre_pool_out.transpose(1,2) # (b,p,32) -> (b,32,p)
         out = self.pool(pre_pool_out_t) # (b,32,p) -> (b,32,1)
 
-        return out.transpose(1,2).expand(self.batch_size, self.n_points, 32)
+        return out.transpose(1,2) # (b,32,1) -> (b,1,32)
+
+class PlanePredictor(nn.Module):
+    """
+        This class is used to define the Plane Predictor of our Architecture, which will predict the plane parameters of L dynamic planes
+        Architecture design:
+
+            @ INPUT: Tensor of shape (batch_size, num_points, 3) which represent Point Clouds
+
+            > Simple PointNet which learns the global context of the input point clouds
+            > This information is encoded into one global feature by using Max Pooling
+            > 4 Fully Connected Layers with hidden dimension = 32
+            > L Shallow Networks with hidden dimension = 3 which will give us the Predicted Plane Parameters
+            > L Fully Connected Layers with 1 layer and hidden dimension = D (same as point cloud encoder hidden dimension)
+            > Each plane-specific feature is expanded to N x D to match the output of the point cloud encoder, which will be summed together
+
+
+            @ OUTPUT: Tensor of shape (batch_size, num_points, 32) which will be processed into U-Net
+
+    """
+    def __init__(self, in_dim=32, n_points=1024, n_fc=4, L=3):
+        super(PlanePredictor, self).__init__()
+
+        self.pointNet = SimplePointNet(batch_size=1)
+        self.n_points = n_points
+
+        # 4 FC layers with hidden dim = 32
+
+        self.four_fc = nn.ModuleList(
+            [nn.Linear(in_dim, in_dim) for i in range(n_fc)]
+        )
+
+        # Plane parameters (L shallow networks with hidden dim = 3)
+
+        self.first_shallow = nn.Linear(in_dim, 3)
+        self.shallows = nn.ModuleList(
+            [nn.Linear(3, 3) for i in range(L-1)]
+        )
+
+        # L FC layers with hidden dim = 32
+
+        self.first_fc = nn.Linear(3, in_dim)
+        self.L_fc = nn.ModuleList(
+            [nn.Linear(in_dim, in_dim) for i in range(L-1)]
+        )
+
+    def forward(self, x):
+
+        print("input:",x.shape)
+
+        flc = self.pointNet(x) # (b,p,3) -> (b,1,32)
+
+        print("ptn", flc.shape)
+
+        # 4 FC layers with hidden dim = 32
+
+        for fc in self.four_fc:
+            flc = F.relu(fc(flc))  # (b,1,32) -> (b,1,32)
+            print("flc",flc.shape)
+
+        # Plane parameters ( L Shallow Networks )
+
+        first_sh = F.relu(self.first_shallow(flc)) # (b,1,32) -> (b,1,3)
+        print("first shallow", first_sh.shape)
+
+        shal = first_sh
+
+        for s in self.shallows:
+            shal = F.relu(s(shal)) # (b,1,3) -> (b,1,3)
+
+        print("plane params", shal.shape)
+
+        # L FC networks dim = 32
+
+        first_fc_L = F.relu(self.first_fc(shal)) # (b,1,3) -> (b,1,32)
+        print("first fully", first_fc_L.shape)
+
+        L_fully = first_fc_L
+
+        for fc in self.L_fc:
+            L_fully = F.relu(fc(L_fully))  # (b,1,32) -> (b,1,32)
+
+        print("L fully", first_fc_L.shape)
+
+        # Expansion
+
+        out = L_fully.expand(x.shape[0], self.n_points, first_fc_L.shape[-1]) # (b,1,32) -> (b,p,32)
+
+        return out
